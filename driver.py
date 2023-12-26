b48f00cd5b0ab45782ea480220ea9a2dc5250942
Elisa Du <lsdx3442@hochschule-trier.de>
2023-12-26 23:34:57 +0100

    Add data for modeling
    Own recorded data from simulator and the driver starter code

---
+++ driver.py
@@ -0,0 +1,54 @@
+# import tensorflow
+# pip install cv2-contrib-package
+#python-socketio==4.6.1
+# python-engineio==3.13.2
+
+import socketio
+import eventlet
+from flask import Flask
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import cv2
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+speed_limit = 10
+print(speed_limit)
+
+def preprocess_img(img):
+    img = img[60:135, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data={'steering_angle': steering_angle.__str__(), 'throttle': throttle.__str__()})
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = preprocess_img(image)
+    image = np.array([image])
+    speed = float(data['speed'])
+    print(speed)
+    throttle = 1.0 - speed/speed_limit
+    steering_angle = float(model.predict(image))
+    send_control(steering_angle, throttle)
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    send_control(0, 0)
+
+
+if __name__ == '__main__':
+    model = load_model('beta_model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
